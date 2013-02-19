6e79acbddb72bb099f307122ac536f7aad84ed62
vanetix <matmcfarland@gmail.com>
2013-02-19 17:55:12 -0600

    add gameloopthread class, split the game loop into two threads

--- src/raven/Main.java
+++ src/raven/Main.java
@@ -1,67 +1,25 @@
 package raven;
 
-import java.awt.Dimension;
-
+import raven.game.GameLoopThread;
 import raven.game.RavenGame;
-import raven.ui.GameCanvas;
 import raven.ui.RavenUI;
 import raven.utils.Log;
 import raven.utils.Log.Level;
-import raven.utils.MapLoadedException;
 
 public class Main {
 	private static RavenUI ui;
 	private static RavenGame game;
-	
-    public static void main(String args[]) {
-    	Log.setLevel(Level.INFO);
-    	
-    	game = new RavenGame();
-    	ui = new RavenUI(game);
-    	
-    	gameLoop();
+
+	public static void main(String args[]) {
+		Log.setLevel(Level.INFO);
+
+		game = new RavenGame();
+		ui = new RavenUI(game);
+
+		GameLoopThread t1 = new GameLoopThread(game, ui);
+		GameLoopThread t2 = new GameLoopThread(game, ui);
+
+		t1.start();
+		t2.start();
 	}
-    
-	//////////////////////////////////////////////////////////////////////////
-	// Game simulation
-
-	private static void gameLoop() {
-    	
-    	Log.info("raven", "Starting game...");
-    	
-    	long lastTime = System.nanoTime();
-    	
-    	while (true) {
-    		// TODO Resize UI if the map changes!
-    		boolean loadedMap = false;
-
-    		long currentTime = System.nanoTime();
-
-    		try{
-    		game.update((currentTime - lastTime) * 1.0e-9);
-    		}
-    		catch (MapLoadedException e){
-    			loadedMap = true;
-    			ui.dispose();
-    	    	ui = new RavenUI(game);
-
-    		}
-    		// Always dispose the canvas
-    		try {
-    			GameCanvas.startDrawing(game.getMap().getSizeX(), game.getMap().getSizeY(), loadedMap);
-    			loadedMap = false;
-    			game.render();
-    		} finally {
-    			GameCanvas.stopDrawing();
-    		}
-    		
-    		long millisToNextUpdate = Math.max(0, (1000 / 60) - ((System.nanoTime() - currentTime) / 1000000));
-			lastTime = currentTime;
-			try {
-				Thread.sleep(millisToNextUpdate);
-			} catch (InterruptedException e) {
-				break;
-			}
-    	}
-    }
 }
